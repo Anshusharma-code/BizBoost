6361e7771f4647dd417c72ff1b202ea9e73e50d5
Anshusharma-code <sharmaanshu2206@gmail.com>
2026-06-21 22:21:07 +0530

    FIX CROS Origion

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["hire-lens-8bkr.vercel.app"],
+    allow_origins=["https://hire-lens-8bkr.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
