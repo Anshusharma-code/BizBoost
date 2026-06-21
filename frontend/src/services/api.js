a3fc1f2a65ec81cd60c2739b3d6f737040c45e69
Anshusharma-code <sharmaanshu2206@gmail.com>
2026-06-21 19:42:15 +0530

    Update backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://bizboost-f6gu.onrender.com",
 });
 
 api.interceptors.request.use(
